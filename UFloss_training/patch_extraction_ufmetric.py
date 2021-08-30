1aa66512e5ebad6643c5e2cbb7e0d5c8de0022e5
KeWang0622 <kewang@berkeley.edu>
2021-08-30 00:20:30 -0700

    add UFLoss_training

--- UFloss_training/patch_extraction_ufmetric.py
+++ UFloss_training/patch_extraction_ufmetric.py
@@ -3,6 +3,9 @@ import os
 from optparse import OptionParser
 import numpy as np
 from tqdm import tqdm
+import sys
+
+sys.path.append("../DL_Recon_UFLoss/")
 from models.unrolled2D.subsample_fastmri import MaskFunc
 
 # models/unrolled2D/subsample_fastmri
@@ -14,20 +17,6 @@ matplotlib.use("TkAgg")
 import sigpy.plot as pl
 import bart
 import cv2
-
-# import torch
-# import torch.backends.cudnn as cudnn
-# import torch.nn as nn
-# import torch.nn.functional as F
-# from torch import optim
-# from unet.vgg import Vgg16
-# from eval import eval_net
-# # from unet import UNet
-# from unet.unet_model import UNet
-# from utils import get_ids, split_ids, split_train_val, get_imgs_and_masks, batch,get_imgs_and_masks_triple
-# import resnet
-# import sigpy.plot as pl
-# from utils.utils_vgg import gram_matrix
 import h5py
 
 
@@ -42,7 +31,6 @@ def dir2dic(dir_list):
     return output
 
 
-# files_knee_valid[0][:-4].split("_")
 def get_args():
     parser = OptionParser()
     parser.add_option(
@@ -81,31 +69,6 @@ def get_args():
     parser.add_option(
         "-y", "--sy", dest="sy", default=320, type="int", help="image dim: y"
     )
-
-    #     parser.add_option('-e', '--epochs', dest='epochs', default=5, type='int',
-    #                       help='number of epochs')
-    #     parser.add_option('-b', '--batch-size', dest='batchsize', default=10,
-    #                       type='int', help='batch size')
-    #     parser.add_option('--lg', '--learning-rate-generator', dest='lg', default=0.0001,
-    #                       type='float', help='learning rate of generator')
-    #     parser.add_option('--ld', '--learning-rate-discrinimator', dest='ld', default=0.0001,
-    #                       type='float', help='learning rate of discrinimator')
-    #     parser.add_option('-g', '--gpu', action='store_true', dest='gpu',
-    #                       default=False, help='use cuda')
-    #     parser.add_option('-c', '--load', dest='load',
-    #                       default=False, help='load file model: generator')
-    #     parser.add_option('-d', '--loadb', dest='loadb',
-    #                       default=False, help='load file model: discriminator')
-    #     parser.add_option('-s', '--save', dest='save', type='int',
-    #                       default=5, help='saving frequencey')
-    #     parser.add_option('-a', '--gan', dest='gan', type='float',
-    #                       default=0.01, help='GAN factor')
-    #     parser.add_option('-i', '--interval', dest='interval', type='int',
-    #                       default=1, help='Interval of GAN')
-    #     parser.add_option('-n', '--name', dest='name',
-    #                       default='mr', help='name of the expirement')
-    #     parser.add_option('-p', '--per', dest='per', type='float',
-    #                       default=0.05, help='perceptual factor')
     (options, args) = parser.parse_args()
     return options
 
@@ -173,9 +136,7 @@ if __name__ == "__main__":
                 index[t, kp, 0] - n1 : index[t, kp, 0] + n2,
                 index[t, kp, 1] - n1 : index[t, kp, 1] + n2,
             ]
-            #                 if abs(pat).sum() == 0:
-            #                     continue
-            #                     print("Skip")
+
             blur_k = np.random.randint(2, 5)
             noise_l = np.random.rand() * 0.2
             pat2 = cv2.blur(pat.real, (blur_k, blur_k)) + 1j * cv2.blur(
@@ -195,7 +156,6 @@ if __name__ == "__main__":
                 index[t, kp, 1] - n1 : index[t, kp, 1] + n2,
             ]
 
-            #                 pl.ImagePlot(np.concatenate((pat[None,...],pat2[None,...],pat3[None,...],pat4[None,...],pat5[None,...])))
             np.save(
                 target
                 + "patch_data/"
@@ -234,40 +194,3 @@ if __name__ == "__main__":
             )
 
         t += 1
-#         print(t)
-
-
-#     net_generator = UNet(n_channels=1000, n_classes=3)
-#     net_generator = nn.DataParallel(net_generator)
-#     net_discriminator = resnet.resnet18(num_classes=1,inputchannel=3)
-#     net_discriminator = nn.DataParallel(net_discriminator)
-#     if args.load:
-#         net_generator.load_state_dict(torch.load(args.load))
-#         print('Model loaded from {}'.format(args.load))
-#     if args.loadb:
-#         net_discriminator.load_state_dict(torch.load(args.loadb))
-#         print('Model loaded from {}'.format(args.loadb))
-
-#     if args.gpu:
-#         net_generator.cuda()
-#         net_discriminator.cuda()
-#         # cudnn.benchmark = True # faster convolutions, but more memory
-
-#     try:
-#         train_net(net_g=net_generator,
-#                   net_d = net_discriminator,
-#                   epochs=args.epochs,
-#                   batch_size=args.batchsize,
-#                   lr_g=args.lg,
-#                   lr_d = args.ld,
-#                   gpu=args.gpu,
-#                   img_scale=0.5,
-#                   svf = args.save,
-#                   GAN_lamda = args.gan,perceptual_lamda = args.per,opt_fre = args.interval,name = args.name)
-#     except KeyboardInterrupt:
-#         torch.save(net_generator.state_dict(), 'INTERRUPTED.pth')
-#         print('Saved interrupt')
-#         try:
-#             sys.exit(0)
-#         except SystemExit:
-#             os._exit(0)
